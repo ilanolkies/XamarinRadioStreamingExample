7c53084ed3a20fd14b1397c6a279fcc057154c84
Ilan Olkies <ilanolkies@outlook.com>
2018-05-23 00:19:26 -0300

    Implement DataBinding for play, pause and stop buttons.
    - Inherit view model form INotifyPropertyChanged
    - Implement NotifyPropertyChanged
    - Create listened properties
    - Bind the view

--- StreamingExample/StreamingExamplePage.xaml.cs
+++ StreamingExample/StreamingExamplePage.xaml.cs
@@ -6,7 +6,7 @@ namespace StreamingExample
 {
     public partial class StreamingExamplePage : ContentPage
     {
-		private StreamingViewModel ViewModel { get { return (StreamingViewModel)this.BindingContext; } }
+		private StreamingViewModel ViewModel { get => (StreamingViewModel)this.BindingContext; }
 
         public StreamingExamplePage()
         {
@@ -19,22 +19,16 @@ namespace StreamingExample
 		// Callbacks to images tapped
 		private void Play_tapped(object sender, EventArgs e)
 		{
-			Play.IsVisible = false;
-			PauseStop.IsVisible = true;
 			ViewModel.Play();
 		}
 
 		private void Pause_tapped(object sender, EventArgs e)
         {
-			Play.IsVisible = true;
-			PauseStop.IsVisible = false;
 			ViewModel.Pause();
         }
 
 		private void Stop_tapped(object sender, EventArgs e)
         {
-			Play.IsVisible = true;
-			PauseStop.IsVisible = false;
 			ViewModel.Stop();
         }
     }
--- StreamingExample/StreamingViewModel.cs
+++ StreamingExample/StreamingViewModel.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace StreamingExample
 {
-    public class StreamingViewModel
-    {
-		public bool IsPlaying { get; set; } = false;
+	public class StreamingViewModel : INotifyPropertyChanged
+	{
+		public bool DisplayPlay { get => !isPlaying; }
+		public bool DisplayPauseStop { get => isPlaying; }
 
-        public void Play()
+		// INotifyPropertyChanged implementation
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		bool isPlaying;
+		bool IsPlaying
+		{
+			get => isPlaying;
+			set
+			{
+				isPlaying = value;
+				// Notify the property has changed
+				OnPropertyChanged("DisplayPlay");
+				OnPropertyChanged("DisplayPauseStop");
+			}
+		}
+
+		public void Play()
 		{
 			IsPlaying = true;
 		}   
