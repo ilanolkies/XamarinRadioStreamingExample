9ab05bc08270fe00a85eca418c8408f714277d06
Ilan Olkies <ilanolkies@outlook.com>
2018-05-22 23:29:23 -0300

    Add StreamingViewModel
    - StreamingViewModel is the main page binding context

---
+++ StreamingExample/StreamingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace StreamingExample
+{
+    public class StreamingViewModel
+    {
+		public bool IsPlaying { get; set; } = false;
+
+        public void Play()
+		{
+			IsPlaying = true;
+		}   
+
+		public void Pause()
+		{
+			IsPlaying = false;
+		}
+
+        public void Stop()
+		{
+			IsPlaying = false;
+		}
+    }
+}
